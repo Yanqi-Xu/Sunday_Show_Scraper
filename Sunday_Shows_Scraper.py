e47a0bbc025c20bbef7a70ada8bcaff06cf1bd3d
Yanqi Xu <yanqixu@outlook.com>
2019-03-13 13:26:56 -0400

    grab the most recent program

--- Sunday_Shows_Scraper.py
+++ Sunday_Shows_Scraper.py
@@ -1,17 +1,17 @@
 import requests, mechanize
 from bs4 import BeautifulSoup
 
-url_cbs = 'https://www.cbsnews.com/news/full-transcript-of-face-the-nation-on-february-3-2019/'
 
 br = mechanize.Browser()
-br.open(url_cbs)
-html = br.response().read()
+parent_url_nbc = 'https://www.nbcnews.com/meet-the-press/meet-press-transcripts-n51976'
+br.open(parent_url_nbc)
+html_nbc = br.response().read()
 
-soup = BeautifulSoup(html, "html.parser")
+nbc_soup = BeautifulSoup(html_nbc, 'html.parser')
 
-transcript = soup.find('div', {'class':'entry'})
+transcript_nbc = nbc_soup.find('a', {'class':'vilynx_listened'})
 
-clean_transcript = transcript.get_text()
+clean_transcript_nbc = transcript_nbc.get_text()
 
 print clean_transcript
 
